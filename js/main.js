57d6f9551fe7c7524acd9e9fee49c43265a7a38d
Sasha <oneg.m@yandex.ru>
2022-06-01 21:09:18 +0300

    2.17. Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
 function getRandomNumber(minNumber, maxNumber) {
-    if (minNumber >= 0 && maxNumber >= 0) {
-        minNumber = Math.ceil(minNumber);
-        maxNumber = Math.floor(maxNumber);
-        return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-    }
-    console.log('Число должно быть положительным');
+  if (minNumber >= 0 && maxNumber >= 0) {
+    minNumber = Math.ceil(minNumber);
+    maxNumber = Math.floor(maxNumber);
+    return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+  }
+  console.log('Число должно быть положительным');
 }
 
 console.log(getRandomNumber(0, 100));
 
 function getRandomNumberPoint(minNumber, maxNumber, numbersAfterPoint) {
-    if (minNumber >= 0 && maxNumber >= 0) {
-        return Math.floor((Math.random() * (maxNumber - minNumber) + minNumber) * (10 ** numbersAfterPoint)) /
-            (10 ** numbersAfterPoint);
-    }
-    console.log('Число должно быть положительным');
+  if (minNumber >= 0 && maxNumber >= 0) {
+    return Math.floor((Math.random() * (maxNumber - minNumber) + minNumber) * (10 ** numbersAfterPoint)) / (10 ** numbersAfterPoint);
+  }
+  console.log('Число должно быть положительным');
 }
 
 console.log(getRandomNumberPoint(1.1, 1.2, 4));
